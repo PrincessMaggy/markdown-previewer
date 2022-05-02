464e4f834974ebe4aaa678a8ba374cdd365477ed
PrincessMaggy <edoziemagdalene@gmail.com>
2022-05-02 11:42:59 +0100

    progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+import Preview from './components/Preview';
+
+// importing styles
+import './styles/app.scss';
 
 function App() {
+
+  const [text, setText] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>Markdown Previewer</h1>
+        <textarea 
+        name="text" 
+        id="text" 
+        cols="30" 
+        rows="10" 
+        value={text}
+        onChange={(e)=> setText(e.target.value)}
+        className="textarea">
+        </textarea>
+
+        <h3>Output</h3>
+
+        <Preview markdown ={text}/>
       </header>
     </div>
   );
